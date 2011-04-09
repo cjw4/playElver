d7ccd251e5c71680f6b326c0557057021b062ef0
Chris <cjw7488@gmail.com>
2011-04-09 16:11:09 -0500

    Added name to User model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index]
   def index
     @title = "Home"
   end
   
   def topten
     @scores = Scorecard.find(:all, :order => "total ASC", :limit => 10)
+    @i = 1
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110409015544) do
+ActiveRecord::Schema.define(:version => 20110409201510) do
 
   create_table "scorecards", :force => true do |t|
     t.integer  "hole1"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20110409015544) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
